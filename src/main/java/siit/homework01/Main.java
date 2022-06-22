2627b12c84bab261f54cb1bfb4d8723aecbcf581
alexandru.bologa <alexandru.bologa.ext@buildingminds.com>
2022-06-22 20:43:39 +0300

    Add non-confidential info

--- src/main/java/siit/homework01/Main.java
+++ src/main/java/siit/homework01/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Guys and Girls");
-        System.out.println("Checking out prices");
-        System.out.println("How are you?");
-        System.out.println("This is the 101 feature implementation");
+        System.out.println("My password is 12345678");
     }
 }

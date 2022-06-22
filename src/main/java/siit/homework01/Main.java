d1350bc37a0809936e03d3026fb6a43661c1d75b
alexandru.bologa <alexandru.bologa.ext@buildingminds.com>
2022-06-22 20:10:37 +0300

    Made the greeting politically correct

--- src/main/java/siit/homework01/Main.java
+++ src/main/java/siit/homework01/Main.java
@@ -3,6 +3,6 @@ package siit.homework01;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Guys");
+        System.out.println("Hello Guys and Girls");
     }
 }

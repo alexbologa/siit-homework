118777055c92ae538c43efe00cd6d9911a2eebc2
alexandru.bologa <alexandru.bologa.ext@buildingminds.com>
2022-06-22 20:27:59 +0300

    Add info about feature

--- src/main/java/siit/homework01/Main.java
+++ src/main/java/siit/homework01/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Guys and Girls");
         System.out.println("How are you?");
-
+        System.out.println("This is the 101 feature implementation");
     }
 }
